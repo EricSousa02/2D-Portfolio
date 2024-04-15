7bf84aec27cc648fa03f16716ad8fde35724f011
EricSousa02 <ericsousadeandrade@gmail.com>
2024-04-15 17:03:32 -0300

    first commit

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Aqui esta o humilde PC que uso para estudar e construir alguns sites para testar minhas habilidades, se quiser dar uma olhada melhor, acesse meu <a href="https://my-portfolio-liard-iota.vercel.app/" target="_blank">Portfolio</a>!`,
+  "cs-degree": `Este e o meu diploma de Tecnico em Informatica, estou estudando para obter um diploma em Analise e Desenvolvimento de Sistemas`,
+  "sofa-table": `Este e meu sofa e a mesa de centro, onde eu passo a maior parte do tempo estudando e assistindo videos de programacao no YouTube e em plataformas de cursos online como a <a href="https://www.alura.com.br/" target="_blank">Alura</a> e a <a href="https://www.udemy.com/" target="_blank">Udemy</a>!`,
+  tv: `Esta e minha TV, onde assisto filmes e series para relaxar e descansar a mente, alem de jogar alguns jogos de vez em quando`,
+  bed: `Esta e minha cama, onde durmo e descanso para poder estudar e estudar no dia seguinte`,
+  resume: `Esta e minha mesa e nela esta meu curriculo. <a href="https://my-portfolio-liard-iota.vercel.app/Curriculo_de_Eric.pdf" target="_blank">De uma olhada?</a> Entre em contato comigo pelo jslegend@protonmail.com se tiver alguma oportunidade de trabalho interessante!`,
+  projects: `Aqui voce pode visualizar minhas redes sociais como <a href="https://github.com/EricSousa02" target="_blank">Github</a> e <a href="https://www.linkedin.com/in/eric-sousa-de-andrade-171803240/" target="_blank">Linkedln</a>.`,
+  library: `Esta e minha estante de livros, onde guardo meus livros de programacao e tecnologia, alem de livros de ficcao e fantasia para ler nas horas vagas
+  , sei ingles e espanhol, entao se precisar de ajuda com alguma traducao ou algo do tipo, pode contar comigo!`,
+  exit: `Ja vai embora? Feche a aba para sair. Espero que tenha gostado do meu mini portfolio!`,
+};
